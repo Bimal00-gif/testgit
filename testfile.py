b6971af6a4652600c02983bfc0444a4ecfd1f093
Bimal00-gif <118796961+Bimal00-gif@users.noreply.github.com>
2023-04-13 13:04:09 +0545

    Update testfile.py

--- testfile.py
+++ testfile.py
@@ -1,10 +1,8 @@
- #This program adds two numbers
+ print("hello world")
 
-num1 = 1.5
-num2 = 6.3
-
-# Add two numbers
-sum = num1 + num2
-
-# Display the sum
-print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
+try:
+    x = input("Enter number: ")
+    x = x + 1
+    print(x)
+except:
+    print("Invalid input")
